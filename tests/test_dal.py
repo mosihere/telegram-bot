9394994f4bfc4d7884ae4fe57c32ad0b8597abc2
Mosihere <Mostafakhoshhal@yahoo.com>
2024-03-16 20:06:28 +0330

    add unit tests for find_movie_quality/ find_series_season and get_movies_from_db

--- tests/test_dal.py
+++ tests/test_dal.py
@@ -1,7 +1,7 @@
 import os
 import pytest
 import mysql.connector
-from dal import is_duplicate, create_record_for_movies, create_record_for_movie_links, get_movie_data, movie_data_normalizer
+from dal import is_duplicate, create_record_for_movies, movie_data_normalizer, find_movie_quality, find_series_season, get_movies_from_db
 from mysql.connector import errorcode
 
 
@@ -62,6 +62,40 @@ def sample_normalized_movie_data():
     return data
 
 
+@pytest.fixture
+def sample_links():
+    data = [
+        'https://5f8u2z8mn5qjqvfdxs59z5g6aw8djtnew25.kinguploadf2m15.xyz/Film/2023/The.Roundup.No.Way.Out.2023.1080p.WEB-DL.Farsi.Dubbed.New.Film2Media.mkv',
+        'https://5f8u2z8mn5qjqvfdxs59z5g6aw8djtnew25.kinguploadf2m15.xyz/Film/2023/The.Roundup.No.Way.Out.2023.720p.WEB-DL.Farsi.Dubbed.New.Film2Media.mkv',
+        'https://5f8u2z8mn5qjqvfdxs59z5g6aw8djtnew25.kinguploadf2m15.xyz/Film/2023/The.Roundup.No.Way.Out.2023.480p.WEB-DL.Farsi.Dubbed.New.Film2Media.mkv',
+        'https://www.f2m91.fun/15828/damsel-2024/',
+        'https://www.f2m91.fun/category/film/top-movies/',
+        'https://5f8u2z8mn5qjqvfdxs59z5g6aw8djtnew27.kinguploadf2m15.xyz/Serial/The%20Sopranos/S01/',
+        'https://5f8u2z8mn5qjqvfdxs59z5g6aw8djtnew27.kinguploadf2m15.xyz/Serial/The%20Sopranos/S02/',
+    ]
+    return data
+
+
+@pytest.fixture
+def sample_founded_movie_quality():
+    data = [
+        '1080p',
+        '720p',
+        '480p',
+    ]
+    return data
+
+
+@pytest.fixture
+def sample_founded_series_season():
+    data = [
+        'S01',
+        'S02',
+        'S03',
+    ]
+    return data
+
+
 def test_is_duplicate():
     """
     Test if is_duplicate function works properly.
@@ -85,3 +119,29 @@ def test_movie_data_normalizer(sample_json_movie_data, sample_normalized_movie_d
     """
 
     assert movie_data_normalizer(sample_json_movie_data) == sample_normalized_movie_data
+
+
+def test_find_movie_quality(sample_links, sample_founded_movie_quality):
+    """
+    Test if find_movie_quality function works properly.
+    """
+
+    assert find_movie_quality(sample_links[0]) == [sample_founded_movie_quality[0]]
+    assert find_movie_quality(sample_links[4]) == []
+
+
+def test_find_series_season(sample_links, sample_founded_series_season):
+    """
+    Test if find_series_season function works properly.
+    """
+
+    assert find_series_season(sample_links[5]) == [sample_founded_series_season[0]]
+    assert find_series_season(sample_links[6]) == [sample_founded_series_season[1]]
+
+
+def test_get_movies_from_db():
+    """
+    Test if get_movies_from_db function works properly.
+    """
+    
+    assert get_movies_from_db() == [(4838, 'the-zone-of-interest-2023', 'https://www.f2m90.fun/15726/the-zone-of-interest-2023', None)]
