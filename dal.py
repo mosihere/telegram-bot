6ae9e15c4984ef0e16e0217cd3683622b3577766
Mosihere <Mostafakhoshhal@yahoo.com>
2024-09-07 18:31:00 +0330

    get_movie_poster and set_movie_poster functions move to data access layer (dal) / fix typo in log for extracted links

--- dal.py
+++ dal.py
@@ -1,15 +1,16 @@
 import os
 import re
 import time
-from datetime import datetime
 import aiohttp
 import requests
 import mysql.connector
-from tabulate import tabulate
 from typing import List, Dict
+from tabulate import tabulate
+from datetime import datetime
 from mysql.connector import errorcode
 
 
+BASE_URL = "https://www.f2mex.ir"
 MOVIE_INFO_URL = 'https://www.omdbapi.com'
 
 
@@ -241,6 +242,62 @@ def create_record_for_movie_links(val: tuple) -> int | str:
     
     except mysql.connector.Error as err:
         return f'We faced an error: {err}'
+
+
+def get_movie_poster_url(record: tuple) -> tuple | None:
+    """
+    Get a single argument as record
+    Unpack result and assign them to variables
+    send a get request to given URL
+    find movie poster
+    return that poster_url
+
+    Args:
+        record: tuple
+    
+    Returns:
+        tuple(str, str) | None
+    """
+
+    movie_id = record[0]
+    movie_url = record[2]
+    movie_site_id = movie_url.split('/')[3]
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}
+    response = requests.get(movie_url, headers=headers)
+    pattern = rf'{BASE_URL}/wp-content/uploads/poster/.*/{movie_site_id}-[a-zA-Z0-9]+\.webp'
+    match = re.search(pattern, response.text)
+
+    if match:
+        poster_url = match.group(0)
+        return poster_url, movie_id
+
+    return None
+
+
+def set_movie_poster(movie_id: str, poster_url: str) -> None:
+    """
+    UPDATE movies_movie table
+    SET poster_url for record
+
+    Args:
+        poster_url: str
+        movie_id: str
+
+    Returns:
+        None
+    """
+
+    sql_command = """
+        UPDATE movies_movie
+        SET poster_url = %s
+        WHERE id = %s
+        """
+    
+    conx = connect_to_database()
+    cursor = conx.cursor()
+    cursor.execute(sql_command, (poster_url, movie_id))
+    conx.commit()
+    conx.close()
 
 
 def get_movie_data(record: tuple) -> bool:
@@ -488,7 +545,7 @@ if __name__ == '__main__':
             update_last_movie_id(movies[-1][0])
         except IndexError:
             pass
-        print(f'{len(movies)} Movies/Series Links Extracted In Date: {year:04d}-{month:02d}-{day:02d}\nTime: {hour:02d}:{minute:02d}:{second:02d}\n')
+        print(f'{len(movies)} Movies/Series Links Extracted On: {year:04d}-{month:02d}-{day:02d}\nTime: {hour:02d}:{minute:02d}:{second:02d}\n')
 
     else:
         print('No movies found to process.')
