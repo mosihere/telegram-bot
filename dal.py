f0aa5b7d8090371d9b3748e1f6bbd03abf92fa90
Mosihere <Mostafakhoshhal@yahoo.com>
2024-09-11 02:57:18 +0330

    move link_extraction process to get_links module

--- dal.py
+++ dal.py
@@ -578,30 +578,3 @@ def movie_links_endpoint(movie_id: int) -> dict:
 
     response = requests.get(f'http://127.0.0.1:8000/api/links/?movie_id={movie_id}')
     return response.json()
-
-
-
-if __name__ == '__main__':
-    datetime_info = get_datetime_info()
-
-    year = datetime_info.get('year')
-    month = datetime_info.get('month')
-    day = datetime_info.get('day')
-    hour = datetime_info.get('hour')
-    minute = datetime_info.get('minute')
-    second = datetime_info.get('second')
-
-    movies = get_movies_from_db()
-
-    if movies:
-        for movie in movies:
-            series_links = get_series_data(movie)
-            movie_links = get_movie_data(movie)
-        try:
-            update_last_movie_id(movies[-1][0])
-        except IndexError:
-            pass
-        print(f'{len(movies)} Movies/Series Links Extracted On: {year:04d}-{month:02d}-{day:02d}\nTime: {hour:02d}:{minute:02d}:{second:02d}\n')
-
-    else:
-        print('No movies found to process.')
