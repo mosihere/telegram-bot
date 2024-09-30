1f967425a7151d27ddacf334422498fda985257c
Mosihere <Mostafakhoshhal@yahoo.com>
2024-10-01 01:00:49 +0330

    now Constant have separate module

---
+++ constants.py
@@ -0,0 +1,12 @@
+import os
+
+
+
+MOVIE_INFO_URL = 'https://www.omdbapi.com'
+BASE_URL = 'https://www.f2mex.ir'
+SUBTITLE_URL = 'https://subtitlestar.com/'
+PERSIAN_PREFIX = 'زیرنویس-فارسی-'
+ENGLISH_PREFIX = 'persian-subtitles-'
+API_KEY = os.environ.get('API_KEY')
+TMDB_BEARER_TOKEN = os.environ.get('TMDB_BEARER_TOKEN')
+BOT_USERNAME = '@shodambot'
