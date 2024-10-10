58f40394aa161653d6026d9a0e6d8e72a56e4013
Mosihere <Mostafakhoshhal@yahoo.com>
2024-10-10 23:59:50 +0330

    better error handling to fix a bug (now when bot facing TimeOutError, User Records No longer delete)

--- messengers.py
+++ messengers.py
@@ -1,4 +1,5 @@
 import asyncio
+from telegram.error import TimedOut, NetworkError, Forbidden
 from bot_instance import bot
 from utils import get_datetime_info
 from dal import get_all_users_telegram_ids, remove_user_from_db
@@ -35,9 +36,16 @@ async def send_message_to_all_users(message: str, reply_markup=None, parse_mode=
             user_database_id = element[1]
             await bot.bot.send_message(chat_id=telegram_id, text=message, reply_markup=reply_markup, parse_mode=parse_mode)
             print(f'Message sent to {telegram_id}')
-        except Exception as e:
+        except TimedOut:
+            print(f'Timeout error while sending message to {telegram_id}. Skipping user for now.')
+        except Forbidden:
+            # When the bot is blocked, delete the user from the database
             remove_user_from_db(telegram_id=telegram_id, user_database_id=user_database_id)
-            print(f'Failed to send message to {telegram_id}: {e}\n So The User and all related Records are Deleted!')
+            print(f'Failed to send message to {telegram_id} (bot blocked). User deleted!')
+        except NetworkError as e:
+            print(f'Network error: {e}. Retrying later for {telegram_id}.')
+        except Exception as e:
+            print(f'Unexpected error for {telegram_id}: {e}')
     print()
 
 if __name__ == '__main__':
